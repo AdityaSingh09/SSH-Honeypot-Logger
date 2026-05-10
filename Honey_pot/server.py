e59944e72f6a5253b5e2ef49f0a20a04b175885e
AdityaSingh09 <adi02090901@gmail.com>
2026-05-11 07:13:12 +1000

    made server.py with local server addresss

---
+++ Honey_pot/server.py
@@ -0,0 +1,73 @@
+import socket
+import threading
+import paramiko
+from datetime import datetime
+
+HOST_KEY = paramiko.RSAKey.generate(2048)
+
+
+class SSHHoneypot(paramiko.ServerInterface):
+
+    def check_auth_password(self, username, password):
+
+        print("\n=== LOGIN ATTEMPT ===")
+        print(f"Time: {datetime.now()}")
+        print(f"Username: {username}")
+        print(f"Password: {password}")
+
+        return paramiko.AUTH_FAILED
+
+    def get_allowed_auths(self, username):
+        return "password"
+
+
+def handle_connection(client_socket, address):
+
+    print(f"\n[+] Connection received from {address[0]}")
+
+    transport = paramiko.Transport(client_socket)
+    transport.add_server_key(HOST_KEY)
+
+    server = SSHHoneypot()
+
+    try:
+        transport.start_server(server=server)
+
+        channel = transport.accept(20)
+
+        if channel is None:
+            print("[-] No channel opened.")
+            return
+
+    except Exception as e:
+        print(f"[-] Error: {e}")
+
+    finally:
+        transport.close()
+
+
+def start_server(host="127.0.0.1", port=2222):
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_socket.bind((host, port))
+    server_socket.listen(100)
+
+    print(f"[+] SSH Honeypot running on port {port}")
+
+    while True:
+
+        client_socket, address = server_socket.accept()
+
+        client_thread = threading.Thread(
+            target=handle_connection,
+            args=(client_socket, address)
+        )
+
+        client_thread.start()
+
+
+if __name__ == "__main__":
+    start_server()
